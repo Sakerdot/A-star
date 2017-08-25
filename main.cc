d4325c51812cda09288d3d5c0a0ea978ad280d68
Adrian <adriansamatan@gmail.com>
2017-08-25 22:20:47 +0200

    main program example

---
+++ main.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "a-star.h"
+
+std::vector<std::vector<int>> createGraph(int vertices)
+{
+    std::vector<std::vector<int>> graph;
+
+    for (int i = 0; i < vertices; ++i)
+    {
+        std::vector<int> row;
+        for (int j = 0; j < vertices; ++j)
+        {
+            row.push_back(0);
+        }
+
+        graph.push_back(row);
+    }
+
+    for (int i = 0; i < vertices; ++i)
+    {
+        for (int j = i + 1; j < vertices; ++j)
+        {
+            std::cout << i << " length to " << j << " (0 for no connection): ";
+
+            int length;
+            std::cin >> length;
+            std::cout << std::endl;
+
+            graph[i][j] = length;
+            graph[j][i] = length;
+        }
+    }
+
+    return graph;
+}
+
+int main()
+{
+    std::cout << "Number of vertices: ";
+    
+    int vertices;
+    std::cin >> vertices;
+    std::cout << std::endl;
+
+    std::vector<int> path = aStar(createGraph(vertices), 0, vertices - 1);
+
+    for (auto node : path)
+    {
+        std::cout << node;
+
+        if (&node != &path.back())
+        {
+            cout << ", ";
+        }
+    }
+
+    std::cout << std::endl;
+    
+    return 1;
+}
